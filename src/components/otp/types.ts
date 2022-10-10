3e3fe857c043d96d17ef955f830f5a2ddd7d7f54
Pooria Faramarzian <PooriaFaramarzian@gmail.com>
2022-10-10 19:33:39 +0330

    refactor: change `counts` to `fields`

--- src/components/otp/types.ts
+++ src/components/otp/types.ts
@@ -3,6 +3,6 @@ import { HTMLProps } from 'react'
 type HtmlInputProps = Partial<HTMLProps<HTMLInputElement>>
 
 export type OtpProps = Omit<HtmlInputProps, 'onChange'> & {
-  counts?: number
+  fields: number
   onChange: (value: string) => void
 }
